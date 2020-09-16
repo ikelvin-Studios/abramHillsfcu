7819db1af5e31596c0a44c2358c90297daab99bc
Kelvin Morrison <kelvinninja1@gmail.com>
2020-09-16 19:34:26 +0000

    Added: changes and Bug Fixes

--- pages/adminsettings.php
+++ pages/adminsettings.php
@@ -1,27 +1,11 @@
 <?php
     $title = "Settings";
-    
+
 
     include ('includes/header.php');
     $poweradmin = $powersite['admin'];
     $powerroute = $powersite['routing_no'];
 
-    if (isset ($_GET['trans_submit'])) {
-    //echo '<script>alert("hey")</script>';
-    $target_id = $_GET['target_id'];
-
-    //echo '<script>alert("hey'.$target_id.'nknkk")</script>';
-
-    $powertargetqt = mysqli_query($con,"SELECT * FROM `db_users_tb` WHERE `dir` = '$target_id' ");
-    $powertarget = mysqli_fetch_array($powertargetqt);
-    $target_user = $powertarget['acc_user'];
-
-    //echo '<script>alert("hey '.$target_user.'")</script>';
-
-//$momocheckqt = mysqli_query($con,"select * from mtn_momo where wallet = '$targetuserid'");
-      //  $momofetchtarget = mysqli_fetch_array($momocheckqt);
-
-}
 
 
 if (isset($_POST['up_submit'])) {
@@ -37,7 +21,7 @@ if (isset($_POST['up_submit'])) {
     $ac_pass = $_POST['pass'];
     $ac_pass2 = $_POST['pass2'];
     $ac_routing = $_POST['routing_no'];
-    
+
   if ($ac_pass != $ac_pass2) {
 
         // $last_message = '<li class="text-danger">Failed to change Credentials due to invalid Confirmation Password</li>';
@@ -46,9 +30,7 @@ if (isset($_POST['up_submit'])) {
 
         echo '<script>alert("Your Password doesn`t match");window.location.assign("adminsettings.php");</script>';
 
-    }
-
-    if ($acc_pin != $powerpass) {
+    } elseif ($acc_pin != $powerpass) {
 
         $last_message = '<li class="text-danger">Failed to change Credentials due to invalid Confirmation Password</li>';
         $last_errors = $last_message;
@@ -56,16 +38,19 @@ if (isset($_POST['up_submit'])) {
 
         echo '<script>alert("Failed to change Credentials due to invalid Confirmation Password");window.location.assign("adminsettings.php");</script>';
 
+    } else {
+      // code...
+
+        $add_query = mysqli_query($con,"UPDATE `site_tb` SET  `admin` = '$use_name', `pass` ='$ac_pass', `routing_no` = '$ac_routing' WHERE `admin`='$userid'") or die(mysqli_error($con));
+
+        if($add_query){
+            echo '<script>alert("Credentials successfully changed");window.location.assign("adminsettings.php");</script>';
+        }
     }
     //echo '<script>alert("hey '.$target_id.'")</script>';
 
 
 
-    $add_query = mysqli_query($con,"UPDATE `site_tb` SET  `admin` = '$use_name', `pass` ='$ac_pass', `routing_no` = '$ac_routing'") or die(mysqli_error($con));
-
-    if($add_query){
-        echo '<script>alert("Credentials successfully changed")window.location.assign("adminsettings.php");</script>';
-    }
 }
 
 
@@ -87,11 +72,11 @@ if (isset($_POST['up_submit'])) {
             </div>
             <!-- /.row -->
             <div class="row">
-               
+
             </div>
             <!-- /.row -->
             <div class="row">
-                
+
                 <!-- /.col-lg-8 -->
                 <div class="col-lg-4">
                     <div class="panel panel-default" id="account">
@@ -105,27 +90,27 @@ if (isset($_POST['up_submit'])) {
                                 <div class="list-group-item">
                                     <input class="form-control" placeholder="Routing No." name="routing_no" type="number" autofocus autocomplete="off" value="<?= $powerroute ?>">
                                 </div>
-                          
+
                                 <div class="list-group-item">
                                     <input class="form-control" placeholder="Username" name="use_name" type="text" autofocus autocomplete="off" value="<?= $poweradmin ?>">
                                 </div>
                                  <div class="list-group-item">
                                     <input class="form-control" placeholder="New Password" name="pass" type="password" autofocus autocomplete="off" value="">
                                 </div>
-                                
+
                                  <div class="list-group-item">
                                    <input class="form-control" placeholder="Confirm Password" name="pass2" type="password" autofocus autocomplete="off" value="">
                                 </div>
-                                 
+
                                 </div>
-                              
-                               
+
+
                                 <!-- <input type="hidden" name="target_id" value=""> -->
 
                                  <div class="list-group-item">
                                   <a class="form-control btn btn-primary" data-toggle="modal" data-target="#ConfirmModal" >Change Credentials</a>
                                 </div>
-                                
+
                                 <div class="list-group-item">
                                   <input class="form-control" value="Clear All Fields" name="clear_submit" type="submit" autofocus autocomplete="off">
                                 </div>
@@ -140,7 +125,7 @@ if (isset($_POST['up_submit'])) {
                                         <div class="modal-body">
                                             <form role="form">
                             <fieldset>
-                                
+
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Enter Your Old Password" name="pin" type="password" value="">
                                 </div>
@@ -152,7 +137,7 @@ if (isset($_POST['up_submit'])) {
                                 <!-- Change this to a button or input when using this as a form -->
                                <!--  <a data-toggle="modal" data-dismiss="modal" data-target="#RegModal" class="">Are you new here, Register</a> -->
                             </fieldset>
-                        
+
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
@@ -173,8 +158,8 @@ if (isset($_POST['up_submit'])) {
                         </div>
                         <!-- /.panel-body -->
                     </div>
-                    
-                </div> 
+
+                </div>
                 <!-- /.col-lg-4 -->
                 <div class="col-lg-8">
                     <!-- <div class="panel panel-default" id="chart">
@@ -207,8 +192,8 @@ if (isset($_POST['up_submit'])) {
                         <!-- /.panel-body --
                     </div> -->
                     <!-- /.panel -->
-                    
-                    
+
+
             </div>
 
 
